448e43376b92dd30a3c576a785c4674b0d9bbaf5
sagarpatil860 <sagarpatil860@gmail.com>
2022-01-08 00:03:22 +0530

    first commit

--- react_fetchapi_assignment/src/App.js
+++ react_fetchapi_assignment/src/App.js
@@ -1,14 +1,18 @@
+
 import logo from './logo.svg';
 import './App.css';
 import {useState} from "react";
 import Pull from './component/Pull';
 console.log("inside app.js");
+
 function App() {
   
   const [sendurl,setsendurl] = useState("/posts");
   return (
     
-    <><h1>hello there</h1>
+    <>
+   
+    <h1>hello there</h1>
     <form>
       <p>
         <label>Select one option</label>
